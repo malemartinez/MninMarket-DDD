b051d1bd087b531e634367461503faab257befab
ruizjose147 <ruizjose_147@hotmail.com>
2022-03-11 23:46:56 -0500

    Se corrigen comandos de AR OrdenPedido

--- src/main/java/co/com/sofka/domain/ordenPedido/comandos/ActualizarNombreProveedor.java
+++ src/main/java/co/com/sofka/domain/ordenPedido/comandos/ActualizarNombreProveedor.java
@@ -2,23 +2,31 @@ package co.com.sofka.domain.ordenPedido.comandos;
 
 import co.com.sofka.domain.generic.Command;
 import co.com.sofka.domain.ordenPedido.Proveedor;
+import co.com.sofka.domain.ordenPedido.valor.Nombre;
 import co.com.sofka.domain.ordenPedido.valor.OrdenID;
+import co.com.sofka.domain.ordenPedido.valor.ProveedorId;
 
 public class ActualizarNombreProveedor extends Command {
 
     private final OrdenID ordenID;
-    private final Proveedor proveedor;
+    private final ProveedorId proveedorId;
+    private final Nombre nombre;
 
-    public ActualizarNombreProveedor(OrdenID ordenID, Proveedor proveedor) {
+    public ActualizarNombreProveedor(OrdenID ordenID, ProveedorId proveedorId, Nombre nombre) {
         this.ordenID = ordenID;
-        this.proveedor = proveedor;
+        this.proveedorId = proveedorId;
+        this.nombre = nombre;
     }
 
     public OrdenID getOrdenID() {
         return ordenID;
     }
 
-    public Proveedor getProveedor() {
-        return proveedor;
+    public ProveedorId getProveedorId() {
+        return proveedorId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
     }
 }
