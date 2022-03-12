50e7a0b7f94d38cf5c305d0d377a7f07156aeb8a
ruizjose147 <ruizjose_147@hotmail.com>
2022-03-12 00:05:34 -0500

    Se corrigen comandos de AR Carrito

--- src/main/java/co/com/sofka/domain/carrito/comandos/actualizarTelefonoCliente.java
+++ src/main/java/co/com/sofka/domain/carrito/comandos/actualizarTelefonoCliente.java
@@ -2,22 +2,30 @@ package co.com.sofka.domain.carrito.comandos;
 
 import co.com.sofka.domain.carrito.Cliente;
 import co.com.sofka.domain.carrito.valor.CarritoID;
+import co.com.sofka.domain.carrito.valor.ClienteID;
+import co.com.sofka.domain.carrito.valor.Telefono;
 import co.com.sofka.domain.generic.Command;
 
 public class actualizarTelefonoCliente extends Command {
     private final CarritoID ID;
-    private final Cliente cliente;
+    private final ClienteID clienteID;
+    private final Telefono telefono;
 
-    public actualizarTelefonoCliente(CarritoID ID, Cliente cliente) {
+    public actualizarTelefonoCliente(CarritoID ID, ClienteID clienteID, Telefono telefono) {
         this.ID = ID;
-        this.cliente = cliente;
+        this.clienteID = clienteID;
+        this.telefono = telefono;
     }
 
     public CarritoID getID() {
         return ID;
     }
 
-    public Cliente getCliente() {
-        return cliente;
+    public ClienteID getClienteID() {
+        return clienteID;
+    }
+
+    public Telefono getTelefono() {
+        return telefono;
     }
 }
